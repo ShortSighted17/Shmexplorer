0e83410b49aaa3432763f05f5c4e27e299411ef7
ShortSighted17 <roi.nuriel1995@gmail.com>
2025-04-26 02:32:00 +0300

    exercises 1.1 - 1.4

--- Shmexplorer_v1.py
+++ Shmexplorer_v1.py
@@ -6,11 +6,20 @@ class URL:
     
     def __init__(self, url):
         
+        # first we deal with "data" case:
+        if url.startswith("data:"):
+            self.scheme = "data"
+            self.data = url[len("data"):]  # skip "data:"
+            return
+        
         self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https", "file"]
+        assert self.scheme in ["http", "https", "file", "data"]
         
         if self.scheme == "file":
             self.path = url
+        
+        elif self.scheme == "data":
+            self.data = url
         
         else:
             if "/" not in url:
@@ -33,6 +42,11 @@ class URL:
     
     def request(self):
         
+        # handling data:
+        if self.scheme == "data":
+            mediatype, data = self.data.split(",", 1)
+            return data
+            
         # handling local files:
         if self.scheme == "file":
             with open(self.path, encoding="utf8") as f:
@@ -80,15 +94,48 @@ class URL:
     
     
 def show(body):
-    # place holder logic: just prints everything that isn't a tag.
+    # buffer holds characters according to longest special case.
+    # only prints what we have if wer'e sure it's not a special case
+    # at the end of the file, flush the rest of the buffer
     in_tag = False
-    for c in body:
+    buffer = ""
+    entity_map = {
+        "&lt;": "<",
+        "&gt;": ">"
+    }
+    max_entity_len = max(len(e) for e in entity_map.keys())
+    i = 0
+    while i < len(body):
+        c = body[i]
         if c == "<":
             in_tag = True
+            i += 1
+            continue
         elif c == ">":
             in_tag = False
+            i += 1
+            continue
+        
         elif not in_tag:
-            print(c, end="")
+            buffer += c
+            
+            # check for match with entities
+            for entity, replacement in entity_map.items():
+                if buffer.endswith(entity):
+                    buffer = buffer[:-len(entity)] # trim buffer
+                    print(replacement, end="") # print replacement instead
+                    break
+            
+            if len(buffer) > max_entity_len:
+                print(buffer[0], end="") # print oldest character
+                buffer = buffer[1:] # trim buffer
+            
+            i += 1 
+        else:
+            i += 1 # always increment silly
+    
+    print(buffer, end="") # when done, flush the buffer
+
                 
                 
 def load(url):
@@ -97,4 +144,11 @@ def load(url):
     
 if __name__ == "__main__":
     import sys
-    load(URL("https://browser.engineering/examples/example1-simple.html"))
+    
+    
+    if len(sys.argv) > 1:
+        url = (URL(sys.argv[1]))
+    else:
+        url = URL("file:///Users/roinu/OneDrive/שולחן העבודה/Side Projects/Shmexplorer/default.html")
+        
+    load(url)
