578fa57eaa3189a1b27acbe365182317f857e3f9
ShortSighted17 <roi.nuriel1995@gmail.com>
2025-04-26 13:06:05 +0300

    added keep-alive functionality

--- Shmexplorer_v1.py
+++ Shmexplorer_v1.py
@@ -6,14 +6,22 @@ class URL:
     
     def __init__(self, url):
         
-        # first we deal with "data" case:
+        self.view_source = False
+        
+        # handling data scheme:
         if url.startswith("data:"):
             self.scheme = "data"
-            self.data = url[len("data"):]  # skip "data:"
+            self.data = url[len("data:"):]
             return
         
+        # handling view-source scheme:
+        if url.startswith("view-source:"):
+            self.view_source = True
+            url = url[len("view-source:"):]  # skip "view-source"
+
+        
         self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https", "file", "data"]
+        assert self.scheme in ["http", "https", "file", "data", "view-source"]
         
         if self.scheme == "file":
             self.path = url
@@ -52,43 +60,49 @@ class URL:
             with open(self.path, encoding="utf8") as f:
                 return f.read()
         
-        # handling actual internet addresses
-        s = socket.socket(
-            family = socket.AF_INET,
-            type = socket.SOCK_STREAM,
-            proto = socket.IPPROTO_TCP
-        )
+        # handling actual internet addresses:
+        # only create a new socket if there isn't an open one
+        if (self.host, self.port) not in open_sockets.keys():
+            s = socket.socket(
+                family = socket.AF_INET,
+                type = socket.SOCK_STREAM,
+                proto = socket.IPPROTO_TCP
+            )
+            open_sockets[(self.host, self.port)] = s
+        else:
+            s = open_sockets[(self.host, self.port)]
         s.connect((self.host, self.port))
         
         if self.scheme == "https":
             ctx = ssl.create_default_context()
             s = ctx.wrap_socket(s, server_hostname=self.host)
         
+        # create and send request
         request = "GET {} HTTP/1.1\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
+        request += "Connection: keep-alive\r\n"
+        request += "User-Agent: Internet Shmexplorer\r\n"
         request += "\r\n"
         s.send(request.encode("utf8"))
         
-        response = s.makefile("r", encoding = "utf8", newline = "\r\n")
-        statusline = response.readline()
+        # recieve response
+        response = s.makefile("rb", newline=b"\r\n")
+        statusline = response.readline().decode("utf8")
         version, status, explanation = statusline.split(" ", 2)
-        response_headers = {
-            "Host": self.host,
-            "Connection": "close",
-            "User-Agent": "Internet Shmexplorer"
-        }
+        response_headers = {}
         
         while True:
             line = response.readline()
-            if line == "\r\n": break
-            header, value = line.split(":", 1)
+            if line == "\r\n":
+                break
+            header, value = line.decode("utf8").split(":", 1)
             response_headers[header.casefold()] = value.strip()
         
         assert "transfer-encoding" not in response_headers
         assert "content-encoding" not in response_headers
-            
-        content = response.read()
-        s.close()
+        
+        length = int(response_headers["content-length"])
+        content = response.read(length).decode("utf8")
         
         return content
     
@@ -97,6 +111,7 @@ def show(body):
     # buffer holds characters according to longest special case.
     # only prints what we have if wer'e sure it's not a special case
     # at the end of the file, flush the rest of the buffer
+    
     in_tag = False
     buffer = ""
     entity_map = {
@@ -104,6 +119,8 @@ def show(body):
         "&gt;": ">"
     }
     max_entity_len = max(len(e) for e in entity_map.keys())
+    
+    
     i = 0
     while i < len(body):
         c = body[i]
@@ -140,15 +157,21 @@ def show(body):
                 
 def load(url):
     body = url.request()
+    if url.view_source:
+        print(body)
     show(body)
     
 if __name__ == "__main__":
     import sys
     
     
+    # (host, port) -> live socket
+    open_sockets = {}
+    
     if len(sys.argv) > 1:
         url = (URL(sys.argv[1]))
     else:
         url = URL("file:///Users/roinu/OneDrive/שולחן העבודה/Side Projects/Shmexplorer/default.html")
+        # url = URL("data:text/html,Hello world!")
         
     load(url)
